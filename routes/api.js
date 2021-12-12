5d6e9a717b116edb8861556ab2bf490348197104
Dwiki Nuwangsa <dwikinuwangsa@gmail.com>
2021-12-12 09:06:17 +0700

    main

--- routes/api.js
+++ routes/api.js
@@ -203,7 +203,7 @@ router.get('/addapikey', (req, res, next) => {
 
     if (!apikey) return res.json(loghandler.notparam)
     if (!(status && apikeyInput && email && nomorhp && name && age && country && exp)) return res.json(loghandler.notAddApiKey)
-    if (apikey != 'Alphabot') return res.json(loghandler.invalidKey)
+    if (apikey != 'Alphabot1') return res.json(loghandler.invalidKey)
 
     try {
         zahirr.insert({
