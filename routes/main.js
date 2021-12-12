df9bb9537c2bf372436ac0ba1b4a13e3df257bb7
Dwiki Nuwangsa <dwikinuwangsa@gmail.com>
2021-12-12 14:47:18 +0700

    main

--- routes/main.js
+++ routes/main.js
@@ -4,16 +4,13 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.sendFile(__path + '/views/home.html')
+    res.sendFile(__path + '/views/home.php')
 })
 
 router.get('/api', (req, res) => {
-    res.sendFile(__path + '/views/index.html')
+    res.sendFile(__path + '/views/index.php')
 })
 
-router.get('/api/about', (req, res) => {
-    res.sendFile(__path + '/views/about.html')
-})
 
 router.get('/api/news', (req, res) => {
     res.redirect('https://raw.githubusercontent.com/Zhirrr/My-SQL-Results/main/Berita.json')
@@ -24,10 +21,10 @@ router.get('/config', (req, res) => {
         status: true,
         result: {
             prefix : '/',
-            namabot: 'ZhirrrBot',
-            namaowner: 'Zhirrr',
-            instagram: 'zhirr_ajalah',
-            youtube : 'Gak Punya'
+            namabot: 'MyFileArchive - RestApi',
+            namaowner: 'MyFileArchive',
+            instagram: 'MyFileArchive',
+            youtube : '-'
         }
     }
     res.json(config)
